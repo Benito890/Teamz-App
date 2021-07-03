764394be8e90bba6c0adbc9956ef21383e89541b
Benito890 <62507204+Benito890@users.noreply.github.com>
2021-07-03 20:59:10 +0200

    Pulling Front project

---
+++ src/teams.js
@@ -0,0 +1,34 @@
+const teams = [
+  {
+    nomProjet: "Développement de site web React / NodeJS",
+    description:
+      "L'objectif du projet est de développer, un site ecommerce....objectif du projet est de développer, un site ecommerce....",
+    status: "in process",
+  },
+  {
+    nomProjet: "Développement de site web React / NodeJS",
+    description:
+      "L'objectif du projet est de développer, un site ecommerce....",
+    status: "in process",
+  },
+  {
+    nomProjet: "Développement de site web React / NodeJS",
+    description:
+      "L'objectif du projet est de développer, un site ecommerce....",
+    status: "in process",
+  },
+  {
+    nomProjet: "Développement de site web React / NodeJS",
+    description:
+      "L'objectif du projet est de développer, un site ecommerce....",
+    status: "in process",
+  },
+  {
+    nomProjet: "Développement de site web React / NodeJS",
+    description:
+      "L'objectif du projet est de développer, un site ecommerce....",
+    status: "in process",
+  },
+];
+
+export default teams;
